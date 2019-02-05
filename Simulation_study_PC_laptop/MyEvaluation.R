e898577f1d035799129b7dd716cb7715c59099d9
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2019-02-05 19:12:40 +0100

    renamed evaluation function

---
+++ Simulation_study_PC_laptop/MyEvaluation.R
@@ -0,0 +1,13 @@
+#Evaluation Criterion: 
+#to determine the power of the method: 
+#is the p-value of the test significant at 0.05 level?
+#if yes, then the test rejected the null-hypothesis that the difference 
+#between the groups is 0: i.e., the alternative hypothesis is correctly accepted
+#if no, then the null hypothesis is incorrectly not rejected.
+
+MyEvaluation <- function(MyAnalysisResult){
+  res <- ifelse(MyAnalysisResult$p.value < 0.05, 1, 0)
+  # 1 means: the alternative hypothesis is correctly accepted
+  # 0 means: the null hypothesis is incorrectly not rejected
+ return(res)
+  }
