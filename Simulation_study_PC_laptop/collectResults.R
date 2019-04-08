e04c9645df5179b979ded527900e624d4c778d54
Github-MS <Github_ms@fsw.leidenuniv.nl>
2019-04-08 12:48:45 +0200

    Add files via upload

--- Simulation_study_PC_laptop/collectResults.R
+++ Simulation_study_PC_laptop/collectResults.R
@@ -4,7 +4,7 @@
 #first load one workspace, for example, cell 1 of the Design matrix
 #save the number of replications within that cell
 Row <- 1
-load(paste("MyResult", "Row", Row,".Rdata" , sep =""))
+load(file.path("results",paste("MyResult", "Row", Row,".Rdata" , sep ="")))
 K <- nrow(MyResult)
 #Initialize a very large matrix with number of rows = K * TotalCells
 Results_sim <- matrix(NA, ncol = ncol(MyResult), nrow = K*TotalCells)
@@ -16,14 +16,16 @@ for (i in 1:TotalCells){
   Results_sim[(K*(i-1)+1):(i*K), ] <- MyResult
 }
 
-#rbind the Design matrix K times
-Results_des <- do.call(what = rbind, args = replicate(K, Design, simplify = F))
+#repeate the Design matrix K times
+Results_des <- Design[rep(1:nrow(Design),each=K),]
+rownames(Results_des) <- 1:nrow(Results_des)
+
 #rbind the vector 1:K TotalCelss times
 Results_K <- do.call(what = rbind, args = replicate(TotalCells, matrix(c(1:K), ncol = 1), simplify = F))
 #Create the final results matrix
 ResultsSimAll <-as.data.frame(cbind(Results_des, K = Results_K, Results_sim))
 #Give the columns the right name:
-names(ResultsSimAll) <- c("Samp", "Es", "K", "Method1", "Method2")
+names(ResultsSimAll) <- c(names(Design), "K", "Method1", "Method2")
 head(ResultsSimAll)
 
 save(ResultsSimAll, file = "AllResultsSim.Rdata")
